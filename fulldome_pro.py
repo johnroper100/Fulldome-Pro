bcb98ddfcab5bb2de1c5e1b4b74fb846f5e831e4
John Roper <johnroper100@gmail.com>
2017-02-04 21:20:50 -0500

    added the initial files

---
+++ fulldome_pro.py
@@ -0,0 +1,44 @@
+# Copyright 2016 John Roper
+#
+# ##### BEGIN GPL LICENSE BLOCK ######
+#
+# Fulldome Pro is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Fulldome Pro is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Fulldome Pro.  If not, see <http://www.gnu.org/licenses/>.
+# ##### END GPL LICENSE BLOCK #####
+
+bl_info = {
+    "name": "Fulldome Pro",
+    "author": "John Roper",
+    "version": (1, 0, 0),
+    "blender": (2, 78, 0),
+    "location": "3D View > Toolbar > Tools > Fulldome Pro",
+    "description": "Quicly set up your scene for fulldome production",
+    "warning": "",
+    "wiki_url": "http://jmroper.com/",
+    "tracker_url": "mailto:johnroper100@gmail.com",
+    "category": "Workflow"
+}
+
+import bpy
+
+
+def register():
+    
+
+
+def unregister():
+    
+
+
+if __name__ == "__main__":
+    register()
